fb8eaf7d776abc66ed4a8a97c9b0a4b6e737fd19
raldblox <raldblox@outlook.com>
2023-08-10 08:45:32 +0800

    init deploy script for testnet

---
+++ hardhat-workspace/scripts/deploy.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+    const TokenTreatsRegistry = await hre.ethers.getContractFactory("TokenTreatsRegistry");
+    const tokentreats = await TokenTreatsRegistry.deploy();
+
+    await tokentreats.deployed();
+
+    console.log("TokenTreats deployed to", tokentreats.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
