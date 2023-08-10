52fcc0b0b2ce216b1e095d1ce74f5972ecc7eb19
raldblox <raldblox@outlook.com>
2023-08-10 08:44:58 +0800

    config hardhat; added optimism and base network

--- hardhat-workspace/hardhat.config.js
+++ hardhat-workspace/hardhat.config.js
@@ -1,6 +1,52 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-contract-sizer");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    opgoerli: {
+      url: process.env.OPTIMISM_GOERLI_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    opmainnet: {
+      url: process.env.OPTIMISM_MAINNET_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    basemainnet: {
+      url: 'https://mainnet.base.org',
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 1000000000,
+    },
+    // for testnet
+    basegoerli: {
+      url: 'https://goerli.base.org',
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 1000000000,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.API_ETHERSCAN,
+  },
 };
