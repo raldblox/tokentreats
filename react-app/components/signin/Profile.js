318f8bc76e73b515b9ec58dfc93db35a54ddee13
raldblox <raldblox@outlook.com>
2023-08-11 16:26:00 +0800

    Added: connected status

--- react-app/components/signin/Profile.js
+++ react-app/components/signin/Profile.js
@@ -27,13 +27,14 @@ export default function Profile() {
                 <a href="/api/auth/login" className={`inline-block ${user ? "p-1" : "px-3"}  bg-[#00000057] py-1 text-yellow-200 uppercase rounded-full  group-hover:bg-green-700`}>
                     {user ? <img src={user?.picture} alt={user?.name} className='h-6 rounded-full' /> : <>LOG IN</>}
                 </a>
-                <p className='flex items-center pr-1'>
+                <p className='flex items-center pr-1 uppercase'>
                     {user ? user.name : <>ACCOUNT</>}
-                    {user && <a href="/api/auth/logout">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                            <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
-                        </svg>
-                    </a>}
+                    {user && <>{" "}CONNECTED
+                        <a href="/api/auth/logout">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                                <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
+                            </svg>
+                        </a></>}
                 </p>
             </div>
         </div>
