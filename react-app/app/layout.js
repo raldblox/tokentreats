4b36c48b0f6661638cf0c7cc57eff0e11c6069c6
raldblox <raldblox@outlook.com>
2023-08-11 14:29:43 +0800

    Added: UserProvider by Auth0

--- react-app/app/layout.js
+++ react-app/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import '@fontsource/poppins';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 export const metadata = {
   title: 'TokenTreats',
@@ -9,7 +10,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <UserProvider>
+          {children}
+        </UserProvider>
+      </body>
     </html>
   )
 }
