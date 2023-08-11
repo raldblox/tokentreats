fcf085e0c4370a715a20827f9c657ef2457c50ff
raldblox <raldblox@outlook.com>
2023-08-11 15:05:28 +0800

    fixed: auth0 next.js app route

--- react-app/next.config.js
+++ react-app/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+require('dotenv').config();
 
-module.exports = nextConfig
+module.exports = {
+    reactStrictMode: true,
+    env: {
+        WLD_CLIENT_ID: process.env.WLD_CLIENT_ID,
+        WLD_CLIENT_SECRET: process.env.WLD_CLIENT_SECRET,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
+    },
+};
