db213b92e967f592afbb393520b70d72b3fe3581
raldblox <raldblox@outlook.com>
2023-08-11 09:08:06 +0800

    Added: treats attestation functions

--- hardhat-workspace/contracts/Treats.sol
+++ hardhat-workspace/contracts/Treats.sol
@@ -3,40 +3,99 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IEAS, AttestationRequest, AttestationRequestData} from "@ethereum-attestation-service/eas-contracts/contracts/IEAS.sol";
+import {NO_EXPIRATION_TIME, EMPTY_UID} from "@ethereum-attestation-service/eas-contracts/contracts/Common.sol";
 
 contract TokenTreatsRegistry {
     uint256 public treatIds;
     using SafeERC20 for IERC20;
 
-    constructor() {}
+    error InvalidEAS();
+
+    IEAS private immutable _eas;
+
+    constructor(IEAS eas) {
+        if (address(eas) == address(0)) {
+            revert InvalidEAS();
+        }
+
+        // LIST OF EAS ADDRESS
+        // OPTIMISM GOERLI: 0x4200000000000000000000000000000000000020
+        // OPTIMISM MAINNET: 0x4200000000000000000000000000000000000021
+        // ETHEREUM MAINNET: 0xA7b39296258348C78294F95B872b282326A97BDF
+        // SEPOLIA TESTNET: 0x0a7E2Ff54e76B8E6659aedc9103FB21c038050D0
+        // BASE GOERLI: 0x720c2bA66D19A725143FBf5fDC5b4ADA2742682E
+
+        _eas = eas;
+    }
 
     struct Treats {
         uint256[] treatId;
         mapping(uint => address) receiver;
         mapping(uint => address) sender;
         mapping(uint => uint) amount;
+        mapping(uint => string) message;
+        mapping(uint => string) file;
         mapping(uint => bool) isFungible;
         mapping(uint => address) tokenAddress; // @note if fungible, need to specify the desired token as treats
     }
 
     Treats private treats;
     mapping(address => uint[]) myTreats;
-
     mapping(address => IERC20) public fungibleTreats;
 
-    function giveTreats(
+    function createTreats(
         address receiver,
         uint256 amount,
+        string memory message,
+        string memory file,
         bool isFungible
     ) public {
-        require(receiver != address(0));
+        require(
+            receiver != address(0),
+            "TokenTreats: Receiver Can't Be Zero Address"
+        );
+        require(amount > 0, "TokenTreats: Insert Valid Amount");
         treats.treatId.push(treatIds);
         treats.receiver[treatIds] = receiver;
+        treats.message[treatIds] = message;
         treats.sender[treatIds] = msg.sender;
         if (isFungible) {
             treats.isFungible[treatIds] = isFungible;
             treats.amount[treatIds] = amount;
+            treats.file[treatIds] = file;
         }
+        treatIds++;
+    }
+
+    // Optimism Goerli UID#153
+
+    function attestTreats(
+        bytes32 schema,
+        address whoLovesTreats,
+        bool isFungibleTreats,
+        string memory tokenName,
+        address tokenAddress
+    ) external returns (bytes32) {
+        return
+            _eas.attest(
+                AttestationRequest({
+                    schema: schema,
+                    data: AttestationRequestData({
+                        recipient: address(0),
+                        expirationTime: NO_EXPIRATION_TIME,
+                        revocable: true,
+                        refUID: EMPTY_UID,
+                        data: abi.encode(
+                            whoLovesTreats,
+                            isFungibleTreats,
+                            tokenName,
+                            tokenAddress
+                        ),
+                        value: 0
+                    })
+                })
+            );
     }
 
     function processFungibleTreats(
