b23789fa356729abcf1a8461016f32cfe45abefe
raldblox <raldblox@outlook.com>
2023-08-12 22:32:55 +0800

    Added: stablecoin & erc20 test token addresses

---
+++ react-app/libraries/CryptoAddresses.js
@@ -0,0 +1,34 @@
+export const usdt = {
+    OpMainnet: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
+    OpGoerli: "", // No supported USDT on Optimism Goerli
+    EthMainnet: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+    Goerli: "", // No supported USDT on Goerli
+    BaseGoerli: "", // No supported USDT on Base Goerli
+    Base: "" // No supported USDT on Base
+};
+
+
+export const usdc = {
+    OpMainnet: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
+    OpGoerli: "", // No supported USDC on Optimism Goerli
+    EthMainnet: "0x8a9447d0D92B822C4eC4b7B0FfC2CC5C682B8989",
+    Goerli: "", // No supported USDC on Goerli
+    BaseGoerli: "", // No supported USDC on Base Goerli
+    Base: "" // No supported USDC on Base
+};
+
+
+export const dai = {
+    OpMainnet: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063",
+    OpGoerli: "", // No supported DAI on Optimism Goerli
+    EthMainnet: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+    Goerli: "", // No supported DAI on Goerli
+    BaseGoerli: "", // No supported DAI on Base Goerli
+    Base: "" // No supported DAI on Base
+};
+
+export const OptimismgGoerliUselessToken = {
+    OUT1: "0x32307adfFE088e383AFAa721b06436aDaBA47DBE",
+    OUT2: "0xb378eD8647D67b5dB6fD41817fd7a0949627D87a",
+    OUT3: "0x4E6597062c7DC988FBcFE77293D833bAD770C19b",
+};
