ba4a932ff12943d36e28007bdc927ca528901c64
raldblox <raldblox@outlook.com>
2023-08-12 14:32:01 +0800

    Added: erc721 contract template compatible to core

--- hardhat-workspace/contracts/FungibleTreatSwap.sol
+++ hardhat-workspace/contracts/FungibleTreatSwap.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
+
 pragma solidity ^0.8.9;
 pragma abicoder v2;
 
@@ -8,13 +9,11 @@ import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 contract FungibleTreatSwap {
     ISwapRouter public immutable swapRouter;
 
-    // This example swaps tokenIn/tokenOut for single path swaps and tokenIn/tokenOut for multi path swaps.
-
     // address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     // address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     // address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
-    // For this example, we will set the pool fee to 0.3%.
+    // Set the pool fee to 0.3%.
     uint24 public constant poolFee = 3000;
 
     constructor(ISwapRouter _swapRouter) {
--- hardhat-workspace/contracts/NonFungibleFactory.sol
+++ hardhat-workspace/contracts/NonFungibleFactory.sol
@@ -1,6 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.9;
 
-contract NonFungibleFactory {
-    constructor() {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+/// @title Non-Fungible Treats ERC-721 Template
+/// @author raldblox.eth
+/// @dev Personalize by adding more functions
+contract NonFungibleFactory is ERC721 {
+    uint256 private _tokenIds;
+    address private _creator;
+
+    constructor() ERC721("NonFungibleTreats", "NFT") {
+        _creator = msg.sender;
+    }
+
+    function mint(address receiver) public returns (uint256) {
+        // NFT Logics
+        _safeMint(receiver, _tokenIds);
+        _tokenIds++;
+        return _tokenIds;
+    }
+
+    function creator() public view returns (address) {
+        return _creator;
+    }
+
+    // PERSONALIZE THIS NFT COLLECTION BY ADDING MORE FUNCTIONS
 }
