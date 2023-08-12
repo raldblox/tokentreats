d38293ea3def8ee365dfd7ab9b34d0a320963938
raldblox <raldblox@outlook.com>
2023-08-12 22:34:04 +0800

    Added: List of Deployed Contract Addresses

---
+++ react-app/libraries/DeployedAddresses.js
@@ -0,0 +1,14 @@
+export const optimism = {
+    CoreMainnet: "",
+    CoreGoerli: "0xDe58ED7409e96f71A7FC0871162Bb5ba99B3E7f9",
+};
+
+export const base = {
+    CoreMainnet: "",
+    CoreGoerli: "0x42F11590Cd4C5606a7d81cC264Aa3A1307C22f52",
+};
+
+export const zora = {
+    CoreMainnet: "",
+    CoreGoerli: "",
+};
