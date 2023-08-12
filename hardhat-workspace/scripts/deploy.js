0db4da4571b559bdef3138147ab420238d63fd0f
raldblox <raldblox@outlook.com>
2023-08-12 10:19:46 +0800

    updated deployment script

--- hardhat-workspace/scripts/deploy.js
+++ hardhat-workspace/scripts/deploy.js
@@ -1,10 +1,8 @@
 const hre = require("hardhat");
 
 async function main() {
-    let SchemaRegistry = "0x4200000000000000000000000000000000000020"; // Optimism Goerli UID#153
-    // const eas = "0x23951c363ef8df12e471759896ae5786a65272cc318217e21b0ad8257e845ab9"; // https://optimism-goerli-bedrock.easscan.org/attestation/attestWithSchema/0x23951c363ef8df12e471759896ae5786a65272cc318217e21b0ad8257e845ab9
     const TokenTreatsRegistry = await hre.ethers.getContractFactory("TokenTreatsRegistry");
-    const tokentreats = await TokenTreatsRegistry.deploy(SchemaRegistry);
+    const tokentreats = await TokenTreatsRegistry.deploy();
     await tokentreats.deployed();
     console.log("TokenTreats deployed to", tokentreats.address);
 }
